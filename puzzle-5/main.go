55bcdc1d8d1a0073aa0c5f8d955562f8647a628c
Simon Breitfelder <breitfelder@denic.de>
2023-12-05 13:06:45 +0100

    puzzle 5, part 2

--- puzzle-5/main.go
+++ puzzle-5/main.go
@@ -13,11 +13,19 @@ import (
 func main() {
 	lines := helper.ReadLines("input.txt")
 
-	seeds, mapChain := ParseInput(lines)
-	locations := mapChain.MapManySeedsToLocations(seeds)
-	solution1 := GetLowestValue(locations)
+	seedRanges2, mapChain := ParseInput(lines)
+	seedRanges1 := GetSeedRangesPart1(seedRanges2)
+	locationRanges1 := mapChain.MapSeedRangesToLocationRanges(seedRanges1)
+	locationRanges2 := mapChain.MapSeedRangesToLocationRanges(seedRanges2)
+	solution1 := GetLowestRangeValue(locationRanges1)
+	solution2 := GetLowestRangeValue(locationRanges2)
 
 	fmt.Println("-> part 1:", solution1)
+	fmt.Println("-> part 2:", solution2)
+}
+
+type Range struct {
+	First, Last int
 }
 
 type MapChain struct {
@@ -34,27 +42,34 @@ type Mapping struct {
 	Range              int
 }
 
-func ParseInput(lines []string) ([]int, MapChain) {
-	seeds := ParseSeeds(lines)
+func ParseInput(lines []string) ([]Range, MapChain) {
+	seedRanges := ParseSeedRanges(lines)
 	mapChain := ParseMapChain(lines)
-	return seeds, mapChain
+	return seedRanges, mapChain
 }
 
-func ParseSeeds(lines []string) []int {
+func ParseSeedRanges(lines []string) []Range {
 	if !strings.HasPrefix(lines[0], "seeds:") {
 		helper.ExitWithMessage("no seeds in first line")
 	}
 
 	parts := strings.Split(lines[0][6:], " ")
-	seeds := make([]int, 0)
+	ints := make([]int, 0)
 	for _, p := range parts {
 		if len(p) > 0 {
 			seed, err := strconv.Atoi(p)
 			helper.ExitOnError(err, "invalid seed value %q", p)
-			seeds = append(seeds, seed)
+			ints = append(ints, seed)
 		}
 	}
-	return seeds
+	seedRanges := make([]Range, 0, len(ints)/2)
+	for i := 0; i < len(ints); i += 2 {
+		seedRanges = append(seedRanges, Range{
+			First: ints[i],
+			Last:  ints[i] + ints[i+1] - 1,
+		})
+	}
+	return seedRanges
 }
 
 var patternMappingHeader = regexp.MustCompile(`^(.*)-to-(.*)\s+map:$`)
@@ -84,37 +99,74 @@ func ParseMapChain(lines []string) MapChain {
 	return MapChain{MappingGroups: mappingGroups}
 }
 
-func (mc MapChain) MapManySeedsToLocations(seeds []int) []int {
-	locations := make([]int, 0, len(seeds))
-	for _, seed := range seeds {
-		location := mc.MapSeedToLocation(seed)
-		locations = append(locations, location)
+func (mc MapChain) MapSeedRangesToLocationRanges(seedRanges []Range) []Range {
+	ranges := seedRanges
+	for _, mg := range mc.MappingGroups {
+		ranges = mg.MapRanges(ranges)
 	}
-	return locations
+	return ranges
 }
 
-func (mc MapChain) MapSeedToLocation(seed int) int {
-	val := seed
-	for _, m := range mc.MappingGroups {
-		val = m.MapValue(val)
-	}
-	return val
-}
-
-func (mg MappingGroup) MapValue(src int) int {
+func (mg MappingGroup) MapRanges(srcRanges []Range) []Range {
+	ranges := srcRanges
+	dstRanges := make([]Range, 0)
 	for _, m := range mg.Mappings {
-		if src >= m.SrcStart && src < m.SrcStart+m.Range {
-			return src - m.SrcStart + m.DstStart
+		if len(ranges) == 0 {
+			break
 		}
+
+		mapped, remainder := m.MapRanges(ranges)
+		dstRanges = append(dstRanges, mapped...)
+		ranges = remainder
 	}
-	return src
+	dstRanges = append(dstRanges, ranges...)
+	return dstRanges
 }
 
-func GetLowestValue(values []int) int {
-	min := values[0]
-	for _, v := range values {
-		if v < min {
-			min = v
+func (m Mapping) MapRanges(srcRanges []Range) (dstRanges []Range, remainderRanges []Range) {
+	for _, src := range srcRanges {
+		d, r := m.MapRange(src)
+		dstRanges = append(dstRanges, d...)
+		remainderRanges = append(remainderRanges, r...)
+	}
+	return
+}
+
+func (m Mapping) MapRange(srcRange Range) (dstRanges []Range, remainderRanges []Range) {
+	if srcRange.First > (m.SrcStart + m.Range - 1) {
+		return nil, []Range{srcRange}
+	}
+	if srcRange.Last < m.SrcStart {
+		return nil, []Range{srcRange}
+	}
+	if srcRange.First < m.SrcStart {
+		remainderRanges = append(remainderRanges, Range{First: srcRange.First, Last: m.SrcStart - 1})
+		srcRange.First = m.SrcStart
+	}
+	if srcRange.Last > m.SrcStart+m.Range-1 {
+		remainderRanges = append(remainderRanges, Range{First: m.SrcStart + m.Range, Last: srcRange.Last})
+		srcRange.Last = m.SrcStart + m.Range - 1
+	}
+	return []Range{{
+		First: srcRange.First - m.SrcStart + m.DstStart,
+		Last:  srcRange.Last - m.SrcStart + m.DstStart,
+	}}, remainderRanges
+}
+
+func GetSeedRangesPart1(seedRanges []Range) []Range {
+	seedRanges1 := make([]Range, 0, 2*len(seedRanges))
+	for _, r := range seedRanges {
+		seedRanges1 = append(seedRanges1, Range{First: r.First, Last: r.First})
+		seedRanges1 = append(seedRanges1, Range{First: r.Last - r.First + 1, Last: r.Last - r.First + 1})
+	}
+	return seedRanges1
+}
+
+func GetLowestRangeValue(ranges []Range) int {
+	min := ranges[0].First
+	for _, r := range ranges {
+		if r.First < min {
+			min = r.First
 		}
 	}
 	return min
