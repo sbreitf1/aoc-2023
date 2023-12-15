2c7a0bff943f007050221ddde0b83a25eada51fe
Simon Breitfelder <breitfelder@denic.de>
2023-12-15 08:54:13 +0100

    puzzle 15, part 2

--- puzzle-15/main.go
+++ puzzle-15/main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"aoc/helper"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -13,8 +14,11 @@ func main() {
 
 	initSequences := ParseInitSequences(lines)
 	solution1 := ComputeSumOfHashes(initSequences)
-
 	fmt.Println("-> part 1:", solution1)
+
+	boxes := ComputeBoxes(initSequences)
+	solution2 := ComputePart2(boxes)
+	fmt.Println("-> part 2:", solution2)
 }
 
 func ParseInitSequences(lines []string) []string {
@@ -40,4 +44,61 @@ func HashString(str string) int {
 		hash = (hash * 17) % 256
 	}
 	return hash
+}
+
+type Lens struct {
+	Label       string
+	FocalLength int
+}
+
+func ComputeBoxes(initSequences []string) map[int][]Lens {
+	boxes := make(map[int][]Lens)
+	for _, s := range initSequences {
+		label, action := ParseAction(s)
+		hash := HashString(label)
+		lenses := boxes[hash]
+		if action == "-" {
+			for i := len(lenses) - 1; i >= 0; i-- {
+				if lenses[i].Label == label {
+					lenses = append(lenses[:i], lenses[i+1:]...)
+				}
+			}
+		} else {
+			focalLength, err := strconv.Atoi(action)
+			helper.ExitOnError(err)
+			found := false
+			for i := range lenses {
+				if lenses[i].Label == label {
+					lenses[i].FocalLength = focalLength
+					found = true
+				}
+			}
+			if !found {
+				lenses = append(lenses, Lens{Label: label, FocalLength: focalLength})
+			}
+		}
+		boxes[hash] = lenses
+	}
+	return boxes
+}
+
+func ParseAction(str string) (string, string) {
+	if strings.HasSuffix(str, "-") {
+		return str[:len(str)-1], "-"
+	}
+	parts := strings.Split(str, "=")
+	if len(parts) > 3 {
+		helper.ExitWithMessage("invalid sequence %q", str)
+	}
+	return parts[0], parts[1]
+}
+
+func ComputePart2(boxes map[int][]Lens) int {
+	var sum int
+	for hash, lenses := range boxes {
+		for i, l := range lenses {
+			sum += (hash + 1) * (i + 1) * l.FocalLength
+		}
+	}
+	return sum
 }
